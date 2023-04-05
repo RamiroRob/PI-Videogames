b43355b1ff775666309ea1da24a02e9dd6489a8f
Ramiro Roballos <ramiroroballos@gmail.com>
2023-04-05 17:42:40 -0400

    models de DB

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,12 +1,38 @@
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
+
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
-    name: {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    nombre: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    descripcion: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    plataformas: {
+      type: DataTypes.ARRAY(DataTypes.OBJECT),
+      allowNull: false,
+    },
+    imagen: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    fecha_lanzamiento: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  }, { timestamps: false, tableName: 'videogames' });
 };
