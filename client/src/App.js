716be6a6a9fa688578f06f786d553fb07a9db68a
Ramiro Roballos <ramiroroballos@gmail.com>
2023-04-10 15:56:52 -0400

    landing and home

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Landing from './components/Landing';
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Videogames</h1>
-      <button> Ingresar </button>
+      <Routes>
+        <Route path="/" element={<Landing/>} />
+        <Route path="/home" element={<Home/>} />
+      </Routes>
     </div>
   );
 }
