9b0fff1ca2552481fd2b79ab25e8d960c20f6bd9
Ramiro Roballos <ramiroroballos@gmail.com>
2023-04-27 14:48:28 -0400

    landing page

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import s from './Landing.module.css'
 
 export default function Landing() {
     return (
-        <div>
-            <h1>Henry Videogames</h1>
+        <div className={s.landingContainer}>
+
+            <div className = {s.text}>
+                <h1>Henry Videogames PI</h1>
+                <h2>Ramiro Roballos</h2>
+            </div>
             <Link to="/home">
                 <button> Ingresar</button>
             </Link>
